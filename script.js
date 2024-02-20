3a73a24ff1793aa50c0bd5814dc3496dfc00a453
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-20 16:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,23 +9,18 @@ submit.addEventListener("click", (e) => {
   if (checkbox.checked) {
     localStorage.setItem("username", username.value);
     localStorage.setItem("password", password.value);
-    alert(`Logged in as ${username.value}.`);
   } else {
     localStorage.removeItem("username");
     localStorage.removeItem("password");
-    alert(`Logged in as ${username.value}.`);
+  }
+  alert(`Logged in as ${username.value}.`);
+  if (localStorage.getItem("username")) {
+    if (!document.getElementById("existing")) {
+      form.innerHTML += "<button id='existing'>Login as existing user.</button>";
+      var msg = document.getElementById("existing");
+      msg.addEventListener("click",()=>{
+        alert(`Logged in as ${localStorage.getItem('username')}.`);
+      });
+    }
   }
 });
-
-if(localStorage.getItem("username")){
-    // const node = document.createElement("li");
-    // const textnode = document.createTextNode("Water");
-    // node.appendChild(textnode);
-    // document.getElementById("myList").appendChild(node);
-    form.innerHTML += "<button id='existing'>Login as existing user.</button>"    
-}
-
-var msg = document.getElementById("existing");
-msg.addEventListener("click",()=>{
-     alert(`Logged in as ${localStorage.getItem('username')}.`);
-})
